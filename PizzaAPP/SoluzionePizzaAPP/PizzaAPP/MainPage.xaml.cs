5ba8f129e078d5ffce7e1861da4349c94a1f323a
AntoMolte <antoniomolteni2009@gmail.com>
2026-05-13 12:29:55 +0200

    Soluzione pizza

--- PizzaAPP/SoluzionePizzaAPP/PizzaAPP/MainPage.xaml.cs
+++ PizzaAPP/SoluzionePizzaAPP/PizzaAPP/MainPage.xaml.cs
@@ -4,20 +4,39 @@ namespace PizzaAPP
 {
     public partial class MainPage : ContentPage
     {
-        List<Pizza> pizze = new List<Pizza>();
-
         public MainPage()
         {
             InitializeComponent();
-            pizze.Add(new Pizza("Margherita", 5.99f, "margherita.png", "Pomodoro, mozzarella, basilico"));
-            pizze.Add(new Pizza("Pepperoni", 7.99f, "pepperoni.png", "Pomodoro, mozzarella, pepperoni"));
-            pizze.Add(new Pizza("Vegetariana", 6.99f, "vegetariana.jpg", "Pomodoro, mozzarella, verdure grigliate"));
-            pizze.Add(new Pizza("Hawaiian", 7.49f, "hawaiian.jpg", "Pomodoro, mozzarella, prosciutto cotto, ananas"));
-            pickPizza.ItemsSource = pizze;
-        }  
-        private void OnSelectedIndexChanged(object sender, EventArgs e)
-        {
-            Pizza selectedPizza = (Pizza)pickPizza.SelectedItem;
         }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await CaricaPizze();     // delega la logica di lettura a un metodo separato
+        }
+        private async Task CaricaPizze()
+        {
+            var pizze = new List<Pizza>();
+            using var stream = await FileSystem.OpenAppPackageFileAsync("pizze.txt");
+            using var reader = new StreamReader(stream);
+            while (!reader.EndOfStream)
+            {
+                var riga = await reader.ReadLineAsync();
+                if (string.IsNullOrWhiteSpace(riga)) continue;
+
+                var parti = riga.Split('|');
+                if (parti.Length < 3) continue;
+
+                pizze.Add(new Pizza
+                {
+                    Nome = parti[0].Trim(),
+                    Prezzo = decimal.Parse(parti[1].Trim(),
+                                  System.Globalization.CultureInfo.InvariantCulture),
+                    Ingredienti = parti[2].Trim()
+                });
+            }
+            PizzeList.ItemsSource = pizze;
+        }
+
+
     }
 }
--- PizzaAPP/SoluzionePizzaAPP/PizzaAPP/Models/Pizza.cs
+++ PizzaAPP/SoluzionePizzaAPP/PizzaAPP/Models/Pizza.cs
@@ -8,40 +8,9 @@ namespace PizzaAPP.Models
 {
     public class Pizza
     {
-        private string _nome;
-        private float _prezzo;
-        private string _image;
-        private string _ingredienti;
-        public string Nome
-        {
-            get { return _nome; }
-            set { _nome = value; }
-        }
-
-        public float Prezzo
-        {
-            get { return _prezzo; }
-            set { _prezzo = value; }
-        }
-
-        public string Image
-        {
-            get { return _image; }
-            set { _image = value; }
-        }
-
-        public string Ingredienti
-        {
-            get { return _ingredienti; }
-            set { _ingredienti = value; }
-        }
-
-        public Pizza(string nome, float prezzo, string image, string ingredienti)
-        {
-            _nome = nome;
-            _prezzo = prezzo;
-            _image = image;
-            _ingredienti = ingredienti;
-        }
+        public string Nome { get; set; } = string.Empty;
+        public decimal Prezzo { get; set; }
+        public string Ingredienti { get; set; } = string.Empty;
+        public string PrezzoFormattato => $"Fr {Prezzo:F2}";
     }
 }
